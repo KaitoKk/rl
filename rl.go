e398cfaf80fb576223c3969df3324d9f63a792b3
Kaito Hasegawa <kaitohasegawa@KaitonoMacBook-Air.local>
2023-09-30 18:36:23 +0900

    記事投稿テスト

--- rl.go
+++ rl.go
@@ -1,6 +1,9 @@
 package main
 
-import "rl/clients"
+import (
+	"rl/clients"
+	"rl/models"
+)
 
 const ( // MEMO: 仮置き
 	apiKey = "..."
@@ -15,6 +18,11 @@ func main() {
 		viewId,
 	)
 
-	c.GetDatabase()
+	article := models.Article{
+		Title: "test",
+		Link: "https://example.com",
+	}
+
+	c.PostArticle(article)
 }
 // "https://www.notion.so/1f99b3341629486a961b5a105e8b41d1?v=444d419cb5da4b7e8b26a0d200252a81&pvs=4"
