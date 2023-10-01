990ed3c45c1951423f2eecfb3d1d700f18519576
Kaito Hasegawa <kaitohasegawa@KaitonoMacBook-Air.local>
2023-10-01 18:20:07 +0900

    ”URLをコマンドライン引数から受け取ってURLのヴァリデーションするように変更

--- rl.go
+++ rl.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"os"
+	"regexp"
 	"rl/clients"
 	"rl/models"
 	"rl/scraper"
@@ -15,7 +18,11 @@ const ( // MEMO: 仮置き
 )
 
 func main() {
-	url := "https://example.com"
+	if err := checkArgs(); err != nil {
+		log.Fatalf("Error checking args: %v", err)
+	}
+
+	url := os.Args[1]
 	title, err := scraper.FetchTitle(url)
 	if err != nil {
 		log.Fatalf("Error fetching title: %v", err)
@@ -34,5 +41,22 @@ func main() {
 	}
 
 	c.PostArticle(article)
+}
+
+func checkArgs() error {
+	args := os.Args
+	if len(args) < 2 {
+		return errors.New("引数がありません")
+	}
+
+	if !isValidUrl(args[1]) {
+		return errors.New("URLが不正です")
+	}
+	return nil
+}
+
+func isValidUrl(url string) bool {
+	re := regexp.MustCompile(`^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$`)
+	return re.MatchString(url)
 }
 // "https://www.notion.so/1f99b3341629486a961b5a105e8b41d1?v=444d419cb5da4b7e8b26a0d200252a81&pvs=4"
